520f2290e310bb7a5f31865a83e004a09e75d588
Mathieu Méa <mathieu.mea@gmail.com>
2015-07-13 17:03:35 -0400

    Minor improvements...

--- src/org/mtransit/parser/ca_l_assomption_mrclasso_bus/LAssomptionMRCLASSOBusAgencyTools.java
+++ src/org/mtransit/parser/ca_l_assomption_mrclasso_bus/LAssomptionMRCLASSOBusAgencyTools.java
@@ -35,7 +35,7 @@ public class LAssomptionMRCLASSOBusAgencyTools extends DefaultAgencyTools {
 
 	@Override
 	public void start(String[] args) {
-		System.out.printf("\nGenerating MRCLASSO bus data...\n");
+		System.out.printf("\nGenerating MRCLASSO bus data...");
 		long start = System.currentTimeMillis();
 		this.serviceIds = extractUsefulServiceIds(args, this);
 		super.start(args);
